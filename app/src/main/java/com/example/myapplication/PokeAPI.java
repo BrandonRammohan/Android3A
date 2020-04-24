e35b326dc3439784c5ed7c22df5b14f90a8c2274
brandon <brandon.rammohan@ineoxgroup.com>
2020-04-24 11:35:22 +0200

    Added Call to API REST

---
+++ app/src/main/java/com/example/myapplication/PokeAPI.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface PokeAPI {
+    @GET("/api/v2/pokemon")
+    Call<RestPokemonResponse> getPokemonResponse();
+
+    @GET("/api/v2/ability")
+    Call<RestPokemonResponse> getAbility();
+}
+
